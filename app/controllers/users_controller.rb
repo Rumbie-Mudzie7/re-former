9ebfeebb7461e21a92baecafcbd90ca0fe004b64
rumbie <rumbiemudzie@gmail.com>
2020-09-09 13:10:59 +0200

    Start to build Html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
     def new; end
-    def create; end
+
+    def create
+        @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+
+        if @user.save
+          redirect_to new_user_path
+        else
+          render :new
+        end
+    end
 end
