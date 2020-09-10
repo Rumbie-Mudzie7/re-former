8152e75b67bd23b9ffa0ae18cf6374b74fc80d0e
rumbie <rumbiemudzie@gmail.com>
2020-09-10 11:21:07 +0200

    Implement whitelist params helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,18 @@ class UsersController < ApplicationController
     def new; end
 
     def create
-        @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
-
+        # @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+      @user = User.new(user_params)
         if @user.save
           redirect_to new_user_path
         else
           render :new
         end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:user_name, :email, :password)
     end
 end
